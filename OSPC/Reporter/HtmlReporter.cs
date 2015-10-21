98b746d895c51b02f1b743cb128758d5294a09d3
Arthur Zaczek <zaczek@technikum-wien.at>
2015-10-21 22:18:53 +0200

    Add graphs

--- OSPC/Reporter/HtmlReporter.cs
+++ OSPC/Reporter/HtmlReporter.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZedGraph;
 
 namespace OSPC.Reporter
 {
@@ -14,6 +17,8 @@ namespace OSPC.Reporter
             "#AA0000", "#00AA00", "#0000AA", "#00AAAA", "#AA00AA", "#AAAA00",
             "#880000", "#008800", "#000088", "#008888", "#880088", "#888800",
         };
+        public static readonly RectangleF GraphRect = new RectangleF(0.0f, 0.0f, 512.0f, 256.0f);
+
         private string _outPath;
 
         public HtmlReporter(string outPath = null)
@@ -23,7 +28,7 @@ namespace OSPC.Reporter
 
         public void Create(List<CompareResult> results)
         {
-            if(!Directory.Exists(_outPath))
+            if (!Directory.Exists(_outPath))
             {
                 Directory.CreateDirectory(_outPath);
             }
@@ -39,10 +44,120 @@ namespace OSPC.Reporter
                     WriteResultLine(html, result, diffName);
                     WriteDiff(result, diffName);
                 }
-                html.WriteLine("</table>");
-                html.WriteLine("</body></html>");
+                WriteFooter(html);
                 html.Flush();
             }
+
+            CreateTokenGraph(results);
+            CreateTokenDetailGraph(results);
+            CreatePercentGraph(results);
+        }
+
+        private void CreateTokenGraph(List<CompareResult> results)
+        {
+
+            GraphPane g = new GraphPane(GraphRect, "Verteilung der Token", "-", "Anz. Token");
+            SetupGraph(g);
+            g.XAxis.IsVisible = false;
+
+            var lst = results.Select(i => (double)i.TokenCount).OrderBy(i => i).ToArray();
+
+            var c = g.AddCurve("",
+                Enumerable.Range(1, results.Count).Select(i => (double)i).ToArray(),
+                lst,
+                Color.Red,
+                SymbolType.Circle);
+            c.Line.IsVisible = true;
+            c.Label.IsVisible = false;
+            c.Symbol.IsVisible = false;
+
+            AddLine(g, lst.Average());
+
+            g.AxisChange();
+            using (var img = g.GetImage())
+            {
+                img.Save(Path.Combine(_outPath, "TokenGraph.png"), ImageFormat.Png);
+            }
+        }
+
+        private void CreateTokenDetailGraph(List<CompareResult> results)
+        {
+
+            GraphPane g = new GraphPane(GraphRect, "Verteilung der Token - Top 10%", "-", "Anz. Token");
+            SetupGraph(g);
+            g.XAxis.IsVisible = false;
+
+            int count = (int)((double)results.Count * 0.1);
+
+            var lst = results.Select(i => (double)i.TokenCount).OrderByDescending(i => i).Take(count).OrderBy(i => i).ToArray();
+
+            var c = g.AddCurve("",
+                Enumerable.Range(1, count).Select(i => (double)i).ToArray(),
+                lst,
+                Color.Red,
+                SymbolType.Circle);
+            c.Line.IsVisible = true;
+            c.Label.IsVisible = false;
+            c.Symbol.IsVisible = false;
+
+            AddLine(g, lst.Average());
+
+            g.AxisChange();
+            using (var img = g.GetImage())
+            {
+                img.Save(Path.Combine(_outPath, "TokenDetailGraph.png"), ImageFormat.Png);
+            }
+        }
+
+        private void CreatePercentGraph(List<CompareResult> results)
+        {
+            GraphPane g = new GraphPane(GraphRect, "Verteilung % Anteil", "-", "Anz. Token");
+            SetupGraph(g);
+            g.XAxis.IsVisible = false;
+
+            var lst = results.SelectMany(i => new[] { 100.0 * i.MatchA, 100.0 * i.MatchB }).OrderBy(i => i).ToArray();
+
+            var c = g.AddCurve("",
+                Enumerable.Range(1, results.Count * 2).Select(i => (double)i).ToArray(),
+                lst,
+                Color.Red,
+                SymbolType.Circle);
+            c.Line.IsVisible = true;
+            c.Label.IsVisible = false;
+            c.Symbol.IsVisible = false;
+
+            AddLine(g, lst.Average());
+
+            g.AxisChange();
+            using (var img = g.GetImage(512, 256, 72.0f))
+            {
+                img.Save(Path.Combine(_outPath, "PercentGraph.png"), ImageFormat.Png);
+            }
+        }
+
+        private static void AddLine(GraphPane g, double avg)
+        {
+            var line = new LineObj(0, avg, 1, avg);
+            line.IsClippedToChartRect = true;
+            line.Location.CoordinateFrame = CoordType.XChartFractionYScale;
+            line.Line.Color = Color.Blue;
+            line.Line.Style = System.Drawing.Drawing2D.DashStyle.Dash;
+            g.GraphObjList.Add(line);
+        }
+
+        private static void SetupGraph(ZedGraph.GraphPane g)
+        {
+            g.Legend.IsVisible = true;
+
+            g.XAxis.MajorGrid.IsVisible = true;
+            g.YAxis.MajorGrid.IsVisible = true;
+            g.YAxis.MinorGrid.IsVisible = true;
+
+            g.Title.FontSpec.Size = 18.0f;
+            g.XAxis.Scale.FontSpec.Size = 18.0f;
+            g.YAxis.Scale.FontSpec.Size = 18.0f;
+            g.XAxis.Title.FontSpec.Size = 18.0f;
+            g.YAxis.Title.FontSpec.Size = 18.0f;
         }
 
         private void WriteDiff(CompareResult result, string diffName)
@@ -148,7 +263,7 @@ namespace OSPC.Reporter
 <body>");
             html.WriteLine("<h1>Open Software Plagiarism Checker</h1>");
             html.WriteLine("<p>Created on: {0}</p>", DateTime.Now);
-            html.WriteLine("<table>");
+            html.WriteLine("<table style=\"float: left;\">");
             html.WriteLine(@"<tr>
     <th>A</th>
     <th>% A</th>
@@ -159,5 +274,17 @@ namespace OSPC.Reporter
     <th>Diff</th>
 </tr>");
         }
+        private static void WriteFooter(StreamWriter html)
+        {
+            html.WriteLine("</table>");
+            html.WriteLine("<div style=\"float: left;margin-left: 5px;\">");
+            html.WriteLine("<img src=\"TokenGraph.png\" />");
+            html.WriteLine("<br/>");
+            html.WriteLine("<img src=\"TokenDetailGraph.png\" />");
+            html.WriteLine("<br/>");
+            html.WriteLine("<img src=\"PercentGraph.png\" />");
+            html.WriteLine("</div></body></html>");
+        }
+
     }
 }
